c31e27bce203763ff520830d389067f766647051
KirtanManek <manekkirtan@gmail.com>
2023-08-04 18:51:40 +0530

    LL

--- Lab-Manual/Lab-09/LinkedListImplements.java
+++ Lab-Manual/Lab-09/LinkedListImplements.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 
 class LinkedListMethods {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int a) {
+            this.data = a;
+            this.next = null;
+        }
+    }
     static Node head;
 
-    public void insert(int var) {
+    void insertEnd(int var) {
         Node node = new Node(var);
         if (head == null) {
             head = node;
@@ -16,7 +25,87 @@ class LinkedListMethods {
         }
     }
 
-    public void displayAll() {
+    void insertFront(int var) {
+        Node node = new Node(var);
+        if(head != null){
+            node.next = head;
+        }
+        head = node;
+    }
+
+    void insertPosition(int x, int pos){
+        Node node = new Node(x);
+        if(head == null && pos == 1){
+            head = node;
+        }
+        else if(pos > countNodes()){
+            System.out.println("Invalid Position");
+        }
+        else{
+            int count1 = 1;
+            Node current = head;
+            while(count1 + 1 != pos){
+                current = current.next;
+                count1++;
+            }
+            node.next = current.next;
+            current.next = node;
+        }
+    }
+
+    int deleteEnd(){
+        if(head == null){
+            System.out.println("List Empty");
+            return -1;
+        }
+        else{
+            Node temp = head;
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            int tempData = temp.next.data;
+            temp.next = null;
+            return tempData;
+        }
+    }
+
+    int deleteFront(){
+        if(head == null){
+            System.out.println("List Empty");
+            return -1;
+        }
+        else{
+            int tempData = head.data;
+            head = head.next;
+            return tempData;
+        }
+    }
+
+    int deletePosition(int pos){
+        if(head == null){
+            System.out.println("List Empty");
+            return -1;
+        }
+        else if(pos > countNodes()){
+            System.out.println("Invalid Position");
+            return -1;
+        }
+        else{
+            int count1 = 1;
+            Node save = head;
+            Node pred = head;
+            while(count1 != pos){
+                pred = save;
+                save = save.next;
+                count1++;
+            }
+            int tempData = save.data;
+            pred.next = save.next;
+            return tempData;
+        }
+    }
+
+    void displayAll() {
         //Node current will point to head
         Node current = head;
 
@@ -25,15 +114,16 @@ class LinkedListMethods {
             return;
         }
         System.out.println("Nodes of singly linked list: ");
-        while (current != null) {
+        while (current.next != null) {
             //Prints each node by incrementing pointer
-            System.out.print(current.data + " ");
+            System.out.print("[" + current.data + "]" + "-->");
             current = current.next;
         }
+        System.out.print("[" + current.data + "]");
         System.out.println();
     }
 
-    public void sort() {
+    void sort() {
         Node current = head;
         Node sortNode;
         int temp;
@@ -55,14 +145,19 @@ class LinkedListMethods {
             }
         }
     }
-//for (Node current = head; current != null; current = current.next){}
-    static class Node {
-        int data;
-        Node next;
 
-        Node(int a) {
-            this.data = a;
-            this.next = null;
+    int countNodes(){
+        if(head == null){
+            return 0;
+        }
+        else{
+            int count = 0;
+            Node temp = head;
+            while(temp != null){
+                temp = temp.next;
+                count++;
+            }
+            return count;
         }
     }
 }
@@ -73,9 +168,14 @@ public class LinkedListImplements {
         Scanner input = new Scanner(System.in);
 
         while (true) {
-            System.out.println("To insert press \"1\"");
-            System.out.println("To displayAll press \"2\"");
-            System.out.println("To sort the list press \"3\"");
+            System.out.println("To insert at end press \"1\"");
+            System.out.println("To insert at front press \"2\"");
+            System.out.println("To insert at specific position press \"3\"");
+            System.out.println("To delete from end press \"4\"");
+            System.out.println("To delete from front press \"5\"");
+            System.out.println("To delete from specific position press \"6\"");
+            System.out.println("To displayAll press \"7\"");
+            System.out.println("To sort the list press \"8\"");
             System.out.println("To exit press \"0\"");
             System.out.print("Enter your choice : ");
             int c = input.nextInt();
@@ -83,14 +183,31 @@ public class LinkedListImplements {
             switch (c) {
                 case 1 -> {
                     System.out.print("Enter Number to insert : ");
-                    li.insert(input.nextInt());
+                    li.insertEnd(input.nextInt());
                 }
-                case 2 -> li.displayAll();
-                case 3 -> li.sort();
+                case 2 -> {
+                    System.out.print("Enter Number to insert : ");
+                    li.insertFront(input.nextInt());
+                }
+                case 3 -> {
+                    System.out.print("Enter Number to insert : ");
+                    int x = input.nextInt();
+                    System.out.print("Enter Position : ");
+                    int pos = input.nextInt();
+                    li.insertPosition(x,pos);
+                }
+                case 4 -> System.out.println("Deleted : [" + li.deleteEnd() + "]");
+                case 5 -> System.out.println("Deleted : [" + li.deleteFront() + "]");
+                case 6 -> {
+                    System.out.print("Enter position : ");
+                    int position = input.nextInt();
+                    System.out.println("Deleted : [" + li.deletePosition(position)+ "]");
+                }
+                case 7 -> li.displayAll();
+                case 8 -> li.sort();
                 case 0 -> System.exit(0);
                 default -> System.out.println("Enter valid input.");
             }
-            System.out.println();
             System.out.println();
         }
     }
